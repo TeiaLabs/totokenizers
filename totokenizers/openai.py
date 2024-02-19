a664d5bfb9b19b434e3c68f3fe359fe956426cb8
kristenarguello <kristenarguello20@gmail.com>
2024-02-19 17:40:24 -0300

    Add gpt3.5 turbo 0125 to list

--- totokenizers/openai.py
+++ totokenizers/openai.py
@@ -60,6 +60,7 @@ class OpenAITokenizer:
             "gpt-3.5-turbo-0613",
             "gpt-3.5-turbo-16k-0613",
             "gpt-3.5-turbo-1106",
+            "gpt-3.5-turbo-0125",
             "gpt-4-0314",
             "gpt-4-0613",
             "gpt-4-1106-preview",
