d0f97bb47d1f3caf527f1dbc883c8167cff01a24
Douglas Souza <douglas.souza.002@acad.pucrs.br>
2024-05-09 15:01:40 -0300

    Add in more places :)

--- totokenizers/openai_info.py
+++ totokenizers/openai_info.py
@@ -125,6 +125,7 @@ chat_model_mapping = {
     "gpt-3.5-turbo": "gpt-3.5-turbo-0613",
     "gpt-3.5-turbo-16k": "gpt-3.5-turbo-16k-0613",
     "gpt-4": "gpt-4-0613",
+    "gpt-4-turbo": "gpt-4-turbo-2024-04-09",
     "gpt-4-32k": "gpt-4-32k-0613",
 }
 for alias, target in chat_model_mapping.items():
