e8ec21a7de57ab687f9c57bfb38f3e9b779e3295
Jônatas Wehrmann <jonataswehrmann@gmail.com>
2024-05-14 17:56:28 -0300

    Remove unused code

--- totokenizers/openai.py
+++ totokenizers/openai.py
@@ -53,13 +53,6 @@ class OpenAITokenizer:
             self.count_message_tokens = NotImplementedError
             return
 
-        if self.model in ("gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k", "gpt-4o",):
-            logger.warning(
-                f"'{self.model}' may update over time. Returning num tokens assuming '{self.model}-0613'."
-            )
-            self.model = f"{self.model}-0613"
-            self.model = "gpt-4-0613"
-
         if self.model in {
             "gpt-4-0314",
             "gpt-4-32k-0314",
